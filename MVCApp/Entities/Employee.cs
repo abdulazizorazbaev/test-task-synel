a30c41a346cdcaa119bddc35c692c1f48280d74c
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-10-17 17:26:07 +0500

    Asp.Net MVC project has been created

---
+++ MVCApp/Entities/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace MVCApp.Entities;
+
+public class Employee
+{
+    public long Id { get; set; }
+
+    public string PayrollNumber { get; set; } = String.Empty;
+
+    public string Forename { get; set; } = String.Empty;
+
+    public string Surname { get; set; } = String.Empty;
+
+    public DateOnly DateOfBirth { get; set; }
+
+    public string Telephone { get; set; } = String.Empty;
+
+    public string Mobile { get; set; } = String.Empty;
+
+    public string Address { get; set; } = String.Empty;
+
+    public string Address2 { get; set; } = String.Empty;
+
+    public string Postcode { get; set; } = String.Empty;
+
+    public string Email { get; set; } = String.Empty;
+
+    public DateOnly StartDate { get; set; }
+}
